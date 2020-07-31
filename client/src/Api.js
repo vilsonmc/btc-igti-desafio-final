c4bf4f927bd906c7fd8fb454de2dc87a35fc65a4
vilsonmc <vilsonmaximo@yahoo.com.br>
2020-07-30 22:34:09 -0300

    Alteração no endereço da API

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3001/api/' });
+const api = axios.create({
+  baseURL:
+    'http://localhost:3001/api/' ||
+    'https://vilson-camargo-desafio-final.herokuapp.com',
+});
 
 export default api;
