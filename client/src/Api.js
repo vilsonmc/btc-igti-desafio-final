f931f40e91aca247da58c40e89e84d2c0faf56fe
vilsonmc <vilsonmaximo@yahoo.com.br>
2020-07-30 22:58:37 -0300

    Correção do noma da API

--- client/src/Api.js
+++ client/src/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     'http://localhost:3001/api/' ||
-    'https://vilson-camargo-desafio-final.herokuapp.com',
+    'https://vilson-camargo-desafio-final.herokuapp.com/api/',
 });
 
 export default api;
